48733085370479d5bf154ae650fb31aa0b2c3647
Khun Soe Yee <155002583+khunsoeyee@users.noreply.github.com>
2026-03-29 23:04:43 +0630

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+// sw.js ထဲက assets နေရာမှာ icon.png ထည့်ရန်
+const assets = [
+  'index.html',
+  'manifest.json', // manifest ပါ ထည့်ပေးရင် ပိုကောင်းပါတယ်
+  'icon.png',       // အိုင်ကွန်အသစ်
+  'mm-flag.png',
+  'pao-flag.png',
+  'sun.jpg',
+  'mon.jpg',
+  // ... (ကျန်တဲ့ပုံများ)
+];
